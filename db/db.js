6b6cd380f8c0d4f8c274255dc12c40de988fb75a
Cody Mumford <cody.mumford@gmail.com>
2021-05-12 11:54:32 -0700

    fixed bug

--- db/db.js
+++ db/db.js
@@ -12,7 +12,7 @@ sequelize.authenticate()
     console.log('Connection has been established successfully.');
   })
   .catch(err => {
-    console.error('Unable to connect to the database:', error);
+    console.error('Unable to connect to the database:', err);
   });
 
 module.exports = { sequelize };
