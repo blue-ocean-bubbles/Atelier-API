41dac40128c293e29c896539b8da85f5570bce12
Cody Mumford <cody.mumford@gmail.com>
2021-05-14 10:38:13 -0700

    put logs back in

--- config/config.example.js
+++ config/config.example.js
@@ -2,5 +2,6 @@ const port = YOUR_PORT_HERE;
 const DATABASE = 'DATABASE';
 const DB_USERNAME = 'USERNAME';
 const DB_PASSWORD = 'PASSWORD';
+const DB_ADDRESS = 'IP_ADDRESS';
 
 module.exports = { port, DB_USERNAME, DB_PASSWORD };
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/qa/questions/:product_id', async (req, res) => {
+  console.log(`serving GET request to /qa/questions/${req.params.product_id}`);
   api.getQuestionList(req.params.product_id)
     .then((questionList) => {
       res.json(questionList);
@@ -19,6 +20,7 @@ app.get('/qa/questions/:product_id', async (req, res) => {
 });
 
 app.post('/qa/questions', (req, res) => {
+  console.log('serving POST request to /qa/questions');
   api.addQuestion(req.body)
     .then((data) => {
       res.send(data);
@@ -30,6 +32,7 @@ app.post('/qa/questions', (req, res) => {
 });
 
 app.post('/qa/questions/:question_id/answers', (req, res) => {
+  console.log(`serving POST request to /qa/questions/${req.params.question_id}/answers`);
   api.addAnswer(req.params.question_id, req.body)
     .then((data) => {
       res.send(data);
@@ -41,6 +44,7 @@ app.post('/qa/questions/:question_id/answers', (req, res) => {
 });
 
 app.put('/qa/questions/:question_id/helpful', (req, res) => {
+  console.log(`serving PUT request to /qa/questions/${req.params.question_id}/helpful`);
   api.markQuestionHelpful(req.params.question_id)
     .then((response) => {
       res.send(response);
@@ -52,6 +56,7 @@ app.put('/qa/questions/:question_id/helpful', (req, res) => {
 });
 
 app.put('/qa/answers/:answer_id/helpful', (req, res) => {
+  console.log(`serving PUT request to /qa/answers/${req.params.answer_id}/helpful`);
   api.markAnswerHelpful(req.params.answer_id)
     .then((response) => {
       res.send(response);
@@ -63,6 +68,7 @@ app.put('/qa/answers/:answer_id/helpful', (req, res) => {
 });
 
 app.put('/qa/questions/:question_id/report', (req, res) => {
+  console.log(`serving PUT request to /qa/questions/${req.params.question_id}/report`);
   api.reportQuestion(req.params.question_id)
     .then((response) => {
       res.send(response);
@@ -74,6 +80,7 @@ app.put('/qa/questions/:question_id/report', (req, res) => {
 });
 
 app.put('/qa/answers/:answer_id/report', (req, res) => {
+  console.log(`serving PUT request to /qa/answers/${req.params.answer_id}/report`);
   api.reportAnswer(req.params.answer_id)
     .then((response) => {
       res.send(response);
